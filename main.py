3567d8efbba9638df78577a705830ec7b84c817c
jlmodell <modell.jeff@me.com>
2022-07-22 13:23:09 -0400

    rewritten with build spec file for pyinstaller

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import datetime
 import msoffcrypto
 import os
 import sys
@@ -6,153 +6,98 @@ from shutil import copyfile
 from dotenv import load_dotenv
 import io
 import pandas as pd
-import sqlite3
 
 load_dotenv()
 
 KEY = os.environ.get("KEY", "")
-assert KEY != "", "KEY environment variable is not set"
+assert KEY != "", f"Error::{datetime.now():%Y-%m-%d %H:%M:%S}::`Message: KEY environment variable is not set `"
 
-file = sys.argv[1]
-assert os.path.isfile(file), "File does not exist"
+file = r'/mnt/busse3/fs1/pkg_schedule/Released Schedule.xls'
+if len(sys.argv) > 1:
+    file = sys.argv[1]
 
-decrypted = io.BytesIO()
+print(file)
 
-sql_db = "C:\\temp\\schedule.db"
-
-
-def copy_file(src):
+def copy_file(src) -> str:
     global file
 
-    dst = src.replace(os.path.basename(src), "_" + os.path.basename(src))
+    dst = os.path.join(os.getcwd(), os.path.basename(src))    
 
     copyfile(src, dst)
 
-    print("Copied file: " + src + " to " + dst)
+    print(f"Completed Task::{datetime.now():%Y-%m-%d %H:%M:%S}::`Copied file: {src} to {dst} `")
 
     file = dst
 
     return dst
 
 
-def decrypt_file(src):
-    print("Decrypting file: " + src)
+def decrypt_file(src) -> None:
+    global KEY
 
-    with open(src, "rb") as f:
-        msoffcrypto.OfficeFile(f).load_key(KEY).decrypt(decrypted)
+    dst = os.path.join(os.getcwd(), "_" + os.path.basename(src))
+    new_dst=dst.replace('.xls', '.xlsx')
 
+    assert os.path.exists(src), f"Error::{datetime.now():%Y-%m-%d %H:%M:%S}::`Message: {src} does not exist `"
 
-def create_df():
+    if os.path.exists(dst) or os.path.exists(new_dst):
+        try:
+            os.remove(dst)
+        except:
+            try:
+                os.remove(new_dst)
+            except:
+                pass
+
     try:
-        df = pd.read_excel(decrypted, sheet_name="Schedule", header=[1])
-    except:
-        df = pd.read_excel(file, sheet_name="Schedule", header=[1])
+        with open(src, "rb") as f:
+            bytes = io.BytesIO(f.read())
+        
+        dst_bytes = io.BytesIO()
+        
+        ms = msoffcrypto.OfficeFile(bytes)
+        ms.load_key(password=KEY)
+        ms.decrypt(dst_bytes)
+        print(f"Completed Task::{datetime.now():%Y-%m-%d %H:%M:%S}::`Decrypted file: {src} `")
 
-    print("Created dataframe")
-
-    df = df.loc[
-        (df["Ready"].str.contains("Y"))
-        | (df["Ready"].str.contains("y"))
-        | (df["WC Ready"].str.contains("Y"))
-        | (df["WC Ready"].str.contains("y"))
-    ].copy()
-
-    df.columns = [
-        "requested",
-        "wh_issue_date",
-        "pulled",
-        "posted",
-        "racks",
-        "parts_prep",
-        "ready",
-        "wc_ready",
-        "job_done",
-        "request",
-        "in_parts_prep_by",
-        "l",
-        "run_date_time",
-        "n",
-        "item",
-        "wc",
-        "tooling",
-        "r",
-        "description",
-        "lot",
-        "lot_info",
-        "qty",
-        "comments",
-        "x",
-        "mp",
-        "pallets",
-    ]
-
-    df.fillna("", inplace=True)
-
-    df["wh_issue_date"] = pd.to_datetime(
-        df["wh_issue_date"], errors="coerce"
-    ).dt.strftime("%Y-%m-%d")
-    df["request"] = pd.to_datetime(df["request"], errors="coerce").dt.strftime(
-        "%Y-%m-%d"
-    )
-    df["in_parts_prep_by"] = pd.to_datetime(
-        df["in_parts_prep_by"], errors="coerce"
-    ).dt.strftime("%Y-%m-%d")
-    df["run_date_time"] = pd.to_datetime(
-        df["run_date_time"], errors="coerce"
-    ).dt.strftime("%Y-%m-%d")
-    df["x"] = pd.to_datetime(df["x"], errors="coerce").dt.strftime("%Y-%m-%d")
-
-    df["r"] = df["r"].astype(str)
-    df["qty"] = df["qty"].astype(str)
-    df["mp"] = df["mp"].astype(str)
-    df["pallets"] = df["pallets"].astype(str)
-
-    return df
-
-
-def connect_to_sqlite_and_read_into_df():
-    try:
-        with sqlite3.connect(sql_db) as conn:
-            df = pd.read_sql_query(
-                """SELECT * FROM 'Released Schedule' WHERE lot NOT LIKE '%W' ORDER BY run_date_time DESC""",
-                conn,
-            )
+        df = pd.read_excel(dst_bytes, sheet_name="Schedule")        
 
     except Exception as e:
-        print(e)
-        exit(1)
+        print(f"Error::{datetime.now():%Y-%m-%d %H:%M:%S}::`Message: File was not encrypted `")
+        
+        if os.environ.get("DEBUG", None) == "true":
+            print(f"Details::{datetime.now():%Y-%m-%d %H:%M:%S}::`...` -- EOF")
+            print("\t", e)
+            print("EOF")
+        
+        df = pd.read_excel(src, sheet_name="Schedule")
+    
+    df.to_excel(new_dst, index=False)
+    print(f"Completed Task::{datetime.now():%Y-%m-%d %H:%M:%S}::`File written: {new_dst} `")
 
-    return df
-
-
-def concat_dfs_drop_duplicates_drop_table_insert_table(df1, df2) -> pd.DataFrame:
-    df2 = df2.drop(columns=["id"])
-
-    df = pd.concat([df1, df2], ignore_index=True)
-
-    df = df.drop_duplicates(subset=["lot"])
-
-    print("Original {} -> Updated {}".format(len(df2), len(df)))
-
-    try:
-        with sqlite3.connect(sql_db) as conn:
-
-            df.to_sql("Released Schedule", con=conn, if_exists="replace", index=False)
-
-    except Exception as e:
-        print(e)
-        exit(1)
-
-    return df
+    os.remove(src)
+    print(f"Completed Task::{datetime.now():%Y-%m-%d %H:%M:%S}::`Deleted file: {src} `")
 
 
 if __name__ == "__main__":
+    print(f"Full Task Started::`{datetime.now():%Y-%m-%d %H:%M:%S} `")
+
+    decrypt_file(copy_file(file))
+
+    print(f"Full Task Finished::`{datetime.now():%Y-%m-%d %H:%M:%S} `")
+    print()
+    print("----------------------------------------------------")
+    print()
+    print(f"Test Task Started::`{datetime.now():%Y-%m-%d %H:%M:%S} `")
+
     try:
-        decrypt_file(copy_file(file))
+        df = pd.read_excel(r"_Released Schedule.xlsx")
+        print(f"Completed Task::{datetime.now():%Y-%m-%d %H:%M:%S}::`Result: file was successfully decrypted `")
     except Exception as e:
-        print(e)
+        print(f"Error::{datetime.now():%Y-%m-%d %H:%M:%S}::`Result: file was not decrypted `")
+        if os.environ.get("DEBUG", None) == "true":
+            print(f"Details::{datetime.now():%Y-%m-%d %H:%M:%S}::`...` -- EOF")
+            print("\t", e)
+            print("EOF")
 
-    df = create_df()
-    df2 = connect_to_sqlite_and_read_into_df()
-
-    df3 = concat_dfs_drop_duplicates_drop_table_insert_table(df, df2)
+    print(f"Full Task Finished::`{datetime.now():%Y-%m-%d %H:%M:%S} `")
